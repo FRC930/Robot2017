65127c500f418907668412b40cfb96324a4e627a
kenny <math4ken@gmail.com>
2017-01-21 15:30:52 -0600

    Updated shooter test code. added option to output current from PDP to smart dashboard

--- Shooter Test code/src/org/usfirst/frc/team930/robot/OI.java
+++ Shooter Test code/src/org/usfirst/frc/team930/robot/OI.java
@@ -1,119 +1,39 @@
 package org.usfirst.frc.team930.robot;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.buttons.Button;
 
-import com.ctre.CANTalon;
-import com.ctre.CANTalon.TalonControlMode;
+import org.usfirst.frc.team930.robot.commands.ExampleCommand;
 
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Joystick.AxisType;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	//// CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	//// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
 
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
 
+	//// TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
 
-		public class OI extends SampleRobot {
-			RobotDrive myRobot = new RobotDrive(5, 6);
-			Joystick stick = new Joystick(0);
-			final String defaultAuto = "Default";   //creates the class and the constructors for the motors and the joystick.
-			final String customAuto = "My Auto";
-			SendableChooser<String> chooser = new SendableChooser<>();
-			Spark hello = new Spark(0);     
-			Spark hello1 = new Spark(1);
-			CANTalon Motor1 = new CANTalon(5);
-			CANTalon Motor2 = new CANTalon(6);
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
 
-	double speed2 = 50;
-	boolean Apressed = false;  //Sets variables necessary to operate the buttons.
-	boolean Bpressed = false;
-	boolean Ypressed = false;
-	boolean onoff = false;
-	double clamppos = 1;
-	double clampneg = -1;
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
 
-
-	public OI() {
-		myRobot.setExpiration(0.1);
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
 }
-
-@Override
-	public void robotInit() {
-	chooser.addDefault("Default Auto", defaultAuto);
-	chooser.addObject("My Auto", customAuto);
-	SmartDashboard.putData("Auto modes", chooser);
-	Motor1.changeControlMode(TalonControlMode.PercentVbus);
-	Motor2.changeControlMode(TalonControlMode.PercentVbus); 
-}  //changes the control mode of the motors to PercentVbus
-
-@Override
-	public void autonomous() {    //puts the robot in autonomous mode
-	String autoSelected = chooser.getSelected();
-	System.out.println("Auto selected: " + autoSelected);
-}
-
-@Override
-	  public void operatorControl() {  //puts the robot into teleop mode.
-	  myRobot.setSafetyEnabled(true);
-	  while (isOperatorControl() && isEnabled()) {
-
-
-		  if(stick.getRawButton(4) && (!Bpressed)){
-			  speed2 += 0.025;                      //if the Y button is pressed it increases the speed by 2.5 percent 
-			  Bpressed = true;
-		  } else if((!stick.getRawButton(4)) && Bpressed){
-			  Bpressed = false;
-		  }
-
-		  if(stick.getRawButton(1) && (!Apressed)){
-			  speed2 -= 0.025;  // If the A button is pressed it decreases the speed by 2.5 percent
-			  Apressed = true;
-		  } else if((!stick.getRawButton(1)) && Apressed){
-			  Apressed = false;
-		  }
-
-		  if(stick.getRawButton(2) && (!Ypressed)){
-			  Ypressed = true; //If the B button is pressed it either starts or stops the motor based on whether the motor was on or off
-  			  onoff = !onoff;
-		  } else if((!stick.getRawButton(2)) && Ypressed){
-			  Ypressed = false;
-		  }
-
-		  if (onoff){
-			  Motor1.set(speed2);       // actually turns on the motors 
-			  Motor2.set(speed2*-1.0);
-		  } else {
-			  Motor1.set(0);
-			  Motor2.set(0);
-			  System.out.print("system is off");
-		  }
-		  if(speed2 >= clamppos){ //if the speed is too high it will clamp it and the controller will rumble
-			  speed2 = 1;
-			  stick.setRumble(RumbleType.kLeftRumble, Math.abs(stick.getRawAxis(0)));
-			  stick.setRumble(RumbleType.kRightRumble, Math.abs(stick.getRawAxis(0)));
-		  }
-		  if(speed2<= clampneg){  //if the speed is too low it clamps it and the controller rumbles
-			  speed2 = -1;
-			  stick.setRumble(RumbleType.kLeftRumble, Math.abs(stick.getRawAxis(0)));
-			  stick.setRumble(RumbleType.kRightRumble, Math.abs(stick.getRawAxis(0)));
-		  }
-		  System.err.println(speed2);
-
-
-		  Timer.delay(0.005); // wait for a motor update time
-	  }
- }
-
-
-@Override
-public void test() {
-}
-}
-
-
-
--- Shooter Test code/src/org/usfirst/frc/team930/robot/Robot.java
+++ Shooter Test code/src/org/usfirst/frc/team930/robot/Robot.java
@@ -1,116 +1,123 @@
 
 package org.usfirst.frc.team930.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.TalonSRX;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team930.robot.commands.ExampleCommand;
-import org.usfirst.frc.team930.robot.subsystems.ExampleSubsystem;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	public static OI oi;
+		public class Robot extends SampleRobot {
+			RobotDrive myRobot = new RobotDrive(5, 6);
+			Joystick stick = new Joystick(0);
+			final String defaultAuto = "Default";   //creates the class and the constructors for the motors and the joystick.
+			final String customAuto = "My Auto";
+			SendableChooser<String> chooser = new SendableChooser<>();
+			Spark hello = new Spark(0);     
+			Spark hello1 = new Spark(1);
+			CANTalon Motor1 = new CANTalon(5);
+			CANTalon Motor2 = new CANTalon(6);
+			PowerDistributionPanel PDP = new PowerDistributionPanel();
 
-	Command autonomousCommand;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+	double speed2 = 50;
+	boolean Apressed = false;  //Sets variables necessary to operate the buttons.
+	boolean Bpressed = false;
+	boolean Ypressed = false;
+	boolean onoff = false;
+	double clamppos = 1;
+	double clampneg = -1;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
-	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
-
-	}
-
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
-
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
-	}
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
-	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
-	}
+	public Robot() {
+		myRobot.setExpiration(0.1);
 }
+
+@Override
+	public void robotInit() {
+	chooser.addDefault("Default Auto", defaultAuto);
+	chooser.addObject("My Auto", customAuto);
+	SmartDashboard.putData("Auto modes", chooser);
+	Motor1.changeControlMode(TalonControlMode.PercentVbus);
+	Motor2.changeControlMode(TalonControlMode.PercentVbus); 
+}  //changes the control mode of the motors to PercentVbus
+
+@Override
+	public void autonomous() {    //puts the robot in autonomous mode
+	String autoSelected = chooser.getSelected();
+	System.out.println("Auto selected: " + autoSelected);
+}
+
+@Override
+	  public void operatorControl() {  //puts the robot into teleop mode.
+	  myRobot.setSafetyEnabled(true);
+	  while (isOperatorControl() && isEnabled()) {
+
+
+		  if(stick.getRawButton(4) && (!Bpressed)){
+			  speed2 += 0.025;                      //if the Y button is pressed it increases the speed by 2.5 percent 
+			  Bpressed = true;
+		  } else if((!stick.getRawButton(4)) && Bpressed){
+			  Bpressed = false;
+		  }
+
+		  if(stick.getRawButton(1) && (!Apressed)){
+			  speed2 -= 0.025;  // If the A button is pressed it decreases the speed by 2.5 percent
+			  Apressed = true;
+		  } else if((!stick.getRawButton(1)) && Apressed){
+			  Apressed = false;
+		  }
+
+		  if(stick.getRawButton(2) && (!Ypressed)){
+			  Ypressed = true; //If the B button is pressed it either starts or stops the motor based on whether the motor was on or off
+  			  onoff = !onoff;
+		  } else if((!stick.getRawButton(2)) && Ypressed){
+			  Ypressed = false;
+		  }
+
+		  if (onoff){
+			  Motor1.set(speed2);       // actually turns on the motors 
+			  Motor2.set(speed2*-1.0);
+		  } else {
+			  Motor1.set(0);
+			  Motor2.set(0);
+			  System.out.print("system is off");
+			  SmartDashboard.putNumber("Channel Current's", PDP.getCurrent(14));
+		  }
+		  if(speed2 >= clamppos){ //if the speed is too high it will clamp it and the controller will rumble
+			  speed2 = 1;
+			  stick.setRumble(RumbleType.kLeftRumble, Math.abs(stick.getRawAxis(0)));
+			  stick.setRumble(RumbleType.kRightRumble, Math.abs(stick.getRawAxis(0)));
+		  }
+		  if(speed2<= clampneg){  //if the speed is too low it clamps it and the controller rumbles
+			  speed2 = -1;
+			  stick.setRumble(RumbleType.kLeftRumble, Math.abs(stick.getRawAxis(0)));
+			  stick.setRumble(RumbleType.kRightRumble, Math.abs(stick.getRawAxis(0)));
+		  }
+		  System.err.println(speed2);
+
+
+		  Timer.delay(0.005); // wait for a motor update time
+	  }
+ }
+
+
+@Override
+public void test() {
+}
+}
+
+
+
