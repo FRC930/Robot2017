6fafdc2ee4d4aab063adff4b90b33957b4ebe8bf
Kyle <soupcateatternom@gmail.com>
2017-01-23 19:02:35 -0600

    Spaced and updated gyro code

--- GyroTurn/src/org/usfirst/frc/team930/robot/Robot.java
+++ GyroTurn/src/org/usfirst/frc/team930/robot/Robot.java
@@ -9,75 +9,86 @@ import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Timer;
 
-
-
 public class Robot extends SampleRobot {
 	 
 	   double targetAngle = 0;
-	   boolean Doneturning = false; 
 	   double speed = 0;
-	   boolean turnRight = true;
 	   
+	   boolean turnRight = true;
+	   boolean Doneturning = false; 
+   
+	   AHRS gyro = new AHRS(SerialPort.Port.kUSB);
+	   
+	   //Left Motors on robot
+	   CANTalon LeftFront = new CANTalon(0);
+	   CANTalon LeftMiddle = new CANTalon(1); 
+	   CANTalon LeftBack = new CANTalon(2);
+	   
+	   //Right Motors on robot
+	   CANTalon RightFront = new CANTalon(3);    
+	   CANTalon RightMiddle = new CANTalon(4);
+	   CANTalon RightBack = new CANTalon(5); 
+	   
+	   //Used to get error
 	   double getError(){
 		   return targetAngle - gyro.getAngle();
 	   }
 	   
-	   AHRS gyro = new AHRS(SerialPort.Port.kUSB);
-	   CANTalon LeftFront = new CANTalon(0);
-	   CANTalon LeftMiddle = new CANTalon(1); 
-	   CANTalon LeftBack = new CANTalon(2);
-	   CANTalon RightFront = new CANTalon(3); 
-	   CANTalon RightMiddle = new CANTalon(4);
-	   CANTalon RightBack = new CANTalon(5); 
-	   {
 	   
-		   while(getError() != targetAngle){
-			   if(getError() >= 0){
-				   turnRight = true;
-			   } else {
-				   turnRight = false;
-			   }
-		       if(turnRight){
-		    	  while(getError() <= targetAngle){
-		    		  if(Math.abs(getError()) >  0.6){
-		    		   speed = .6;
-		    		  } else if(Math.abs(getError()) <  0.1){
-		    			  speed = 0.1;
-		    		  } else {
-		    			  speed = getError();
-		    		  }
-		    		  LeftFront.set(speed);
-		    		  LeftMiddle.set(speed);
-		    		  LeftBack.set(speed);
-		    		  RightFront.set(-speed);
-		    		  RightMiddle.set(-speed);
-		    		  RightBack.set(-speed);
-		    	  }
-		    }
-		       else{
-		       	  while(getError() >= targetAngle){
-				    		  if(Math.abs(getError()) >  0.6){
-				    		   speed = .6;
-				    		  } else if(Math.abs(getError()) <  0.1){
-				    			  speed = 0.1;
-				    		  } else {
-				    			  speed = getError();
-				    		  }
-				    		  LeftFront.set(-speed);
-				    		  LeftMiddle.set(-speed);
-				    		  LeftBack.set(-speed);
-				    		  RightFront.set(speed);
-				    		  RightMiddle.set(speed);
-				    		  RightBack.set(speed);
-				    	  }
-				    }
-		   }
-		       
-		    	  
-		    	   Timer.delay(0.005);
+	   while (getError() != targetAngle) {
+		   
+		   if (getError() >= 0) {
+			   turnRight = true;
+		   } else {
+			   turnRight = false;
 		   }
 		   
-		
-	   }
+		   if (turnRight) {
+			   
+			   while (getError() <= targetAngle) {
+				   
+				   if (Math.abs(getError()) >  0.6) {
+		    		   speed = .6;
+				   } else if (Math.abs(getError()) <  0.1) {
+					   speed = 0.1;
+				   } else {
+					   speed = getError();
+				   }
+				   
+				   //Sets left motors to new speed
+				   LeftFront.set(speed);
+				   LeftMiddle.set(speed);
+				   LeftBack.set(speed);
+				   
+				   //Sets right motors to new speed
+				   RightFront.set(-speed);
+				   RightMiddle.set(-speed);
+				   RightBack.set(-speed);
+			   }
+		   } else {
+		       
+			   while (getError() >= targetAngle) {
+				   
+				   //Maths
+				   if (Math.abs(getError()) >  0.6) {
+					   speed = .6;
+				   } else if (Math.abs(getError()) <  0.1) {
+					   speed = 0.1;
+				   } else {
+					   speed = getError();
+				   }
+				   
+				   //Sets new left motor speeds
+				   LeftFront.set(-speed);
+				   LeftMiddle.set(-speed);
+				   LeftBack.set(-speed);
+				   
+				   //Sets new right motor speeds
+				   RightFront.set(speed);
+				   RightMiddle.set(speed);
+				   RightBack.set(speed);
+			   }
 		   }
-	   
+		   Timer.delay(0.005);
+	   }
+}
