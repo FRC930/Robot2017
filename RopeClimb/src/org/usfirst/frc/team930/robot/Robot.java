fa7c1309f854e2f007b4a27731d803cc303dd928
devin132 <milligandm@hotmail.com>
2017-01-24 18:44:37 -0600

    Made rope climb code

--- RopeClimb/src/org/usfirst/frc/team930/robot/Robot.java
+++ RopeClimb/src/org/usfirst/frc/team930/robot/Robot.java
@@ -1,25 +1,18 @@
 package org.usfirst.frc.team930.robot;
 
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a sample program demonstrating how to use an ultrasonic sensor and
- * proportional control to maintain a set distance from an object.
- */
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+
+
 
 public class Robot extends IterativeRobot {
-	// distance in inches the robot wants to stay from an object
 	
-
+	Joystick stick = new Joystick(0);
+	Spark climb1 = new Spark(0);
 	
-	/**
-	 * Tells the robot to drive to a set distance (in inches) from an object
-	 * using proportional control.
-	 */
 	public void teleopPeriodic() {
-		SmartDashboard.putNumber("Hi", 6);
+		climb1.set(stick.getRawAxis(5));
 	}
 }
