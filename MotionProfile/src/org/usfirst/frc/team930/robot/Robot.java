0a83d50791e191caa6760e1b3fdcbe78d5dcc476
SamMurphy1 <samanthamurphy@wi.rr.com>
2017-01-30 20:03:28 -0600

    Cleaned up drive code, updated motion profile

--- MotionProfile/src/org/usfirst/frc/team930/robot/Robot.java
+++ MotionProfile/src/org/usfirst/frc/team930/robot/Robot.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team930.robot;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -16,6 +20,18 @@ public class Robot extends IterativeRobot {
 	final String customAuto = "My Auto";
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
+	
+	Joystick stick = new Joystick(1);
+	
+	CANTalon L1 = new CANTalon(1);
+	CANTalon L2 = new CANTalon(1);
+	CANTalon R1 = new CANTalon(1);
+	CANTalon R2 = new CANTalon(1);
+	
+	
+	//MotionProfileExample example = new MotionProfileExample(L1, L2, R1, R2);
+
+	boolean[] btnsLast = {false,false,false,false,false,false,false,false,false,false};
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -45,6 +61,13 @@ public class Robot extends IterativeRobot {
 		// autoSelected = SmartDashboard.getString("Auto Selector",
 		// defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
+		
+		// Motion profiling
+		L1.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+		L1.reverseSensor(false);
+		R1.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+		R1.reverseSensor(false);
+		
 	}
 
 	/**
@@ -68,6 +91,49 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		boolean [] btns= new boolean [btnsLast.length];
+		for(int i=1; i<btnsLast.length; ++i)
+			btns[i] = stick.getRawButton(i);
+
+		// get the left joystick axis on Logitech Gampead
+		double leftYjoystick = -1 * stick.getY(); // multiple by -1 so joystick forward is positive
+
+		// call this periodically, and catch the output.  Only apply it if user wants to run MP.
+		// example.control();
+		
+		if (btns[5] == false) {
+			
+			// button5 is off so straight drive
+			L1.changeControlMode(TalonControlMode.Voltage);
+			L1.set(12.0 * leftYjoystick);
+			R1.changeControlMode(TalonControlMode.Voltage);
+			R1.set(12.0 * leftYjoystick);
+
+			// example.reset();
+		} 
+		else {
+			
+			// Button5 is held down so switch to motion profile control mode
+			L1.changeControlMode(TalonControlMode.MotionProfile);
+			R1.changeControlMode(TalonControlMode.MotionProfile);
+			
+			// CANTalon.SetValueMotionProfile setOutput = example.getSetValue();
+					
+			// L1.set(setOutput.value);
+			// R1.set(setOutput.value);
+
+			// if btn is pressed and was not pressed last time
+			if( (btns[6] == true) && (btnsLast[6] == false) ) {
+				// user just tapped button 6
+				// example.startMotionProfile();
+			}
+		}
+
+		// save buttons states for on-press detection
+		for(int i=1;i<10;++i) {
+			
+			btnsLast[i] = btns[i];
+		}
 	}
 
 	/**
