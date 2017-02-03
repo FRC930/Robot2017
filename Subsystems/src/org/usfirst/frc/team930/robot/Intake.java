020418f3ef45d852ac23d305576e52128ab65658
kenny <math4ken@gmail.com>
2017-02-02 18:32:20 -0600

    Added intake code to Subsystems project

--- Subsystems/src/org/usfirst/frc/team930/robot/Intake.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Intake.java
@@ -1,4 +1,7 @@
 package org.usfirst.frc.team930.robot;
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Intake implements Runnable {
@@ -6,7 +9,24 @@ public class Intake implements Runnable {
 	public void run(){
 		
 		System.out.println("Intake " + Timer.getFPGATimestamp());
+		//Constructors
 		
+		Joystick stick = new Joystick(1);	//Controller
+		
+		CANTalon intakeSpark = new CANTalon(8);		//Talon
+		
+		boolean toggleOn = false;
+		intakeSpark.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+			
+			if (stick.getRawButton(1) && !toggleOn) {
+					toggleOn = true;
+					intakeSpark.set(-1);
+				}
+			else if(!stick.getRawButton(1) && toggleOn) {
+				toggleOn = false;
+				intakeSpark.set(0);	
+			}	
+		}
 	}
 	
-}
+
