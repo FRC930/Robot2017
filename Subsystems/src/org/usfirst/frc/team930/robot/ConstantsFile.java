f5d365cb9cdd38c79f844c3a957b348cc3895f6c
Tiburon <tiburonbenavides@gmail.com>
2017-02-06 20:21:31 -0600

    ADDED_MORE_ANGRY_CONSTANTS

--- Subsystems/src/org/usfirst/frc/team930/robot/ConstantsFile.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/ConstantsFile.java
@@ -1,5 +1,40 @@
 package org.usfirst.frc.team930.robot;
 
 public class ConstantsFile {
-
+	
+	// ----------PDP CHANNELS----------
+	
+	public static final int PDP_CHANNEL0 = 0;
+	public static final int PDP_CHANNEL1 = 1;
+	public static final int PDP_CHANNEL2 = 2;
+	public static final int PDP_CHANNEL3 = 3;
+	public static final int PDP_CHANNEL4 = 4;
+	public static final int PDP_CHANNEL5 = 5;
+	public static final int PDP_CHANNEL6 = 6;
+	public static final int PDP_CHANNEL7 = 7;
+	public static final int PDP_CHANNEL8 = 8;
+	public static final int PDP_CHANNEL9 = 9;
+	public static final int PDP_CHANNEL10 = 10;
+	public static final int PDP_CHANNEL11 = 11;
+	public static final int PDP_CHANNEL12 = 12;
+	public static final int PDP_CHANNEL13 = 13;
+	public static final int PDP_CHANNEL14 = 14;
+	public static final int PDP_CHANNEL15 = 15;
+	
+	// ----------Talon Motor Channels----------
+	
+	public static final int L1_MOTOR_CHANNEL = 1;
+	public static final int L2_MOTOR_CHANNEL = 2;
+	public static final int L3_MOTOR_CHANNEL = 3;
+	public static final int R1_MOTOR_CHANNEL = 4;
+	public static final int R2_MOTOR_CHANNEL = 5;
+	public static final int R3_MOTOR_CHANNEL = 6;
+	public static final int SHOOTER_MOTOR_CHANNEL = 7;
+	
+	// ----------Spark Motor Channels-----------
+	
+	public static final int INTAKE_MOTOR_CHANNEL = 0;
+	public static final int CLIMBER_MOTOR_CHANNEL = 9;
+	public static final int FEEDER_MOTOR_CHANNEL = 10;
+	
 }
