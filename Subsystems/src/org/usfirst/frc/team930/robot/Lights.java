fa875aa8c58574caef5a229afb51928e48a0b943
devin132 <milligandm@hotmail.com>
2017-02-08 17:02:20 -0600

    lights

--- Subsystems/src/org/usfirst/frc/team930/robot/Lights.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Lights.java
@@ -11,7 +11,7 @@ public class Lights implements Runnable {
 		}
 		
 		//Sets the teleop lights when no other lights are running
-		if(Math.abs(DSManager.getDriveYAxis()) <= Constants.JOYSTICK_ERROR_ALLOWANCE && DSManager.getCoDriveRawButtonOne() && OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)>5 && Math.abs(Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY)) < Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY)) < Constants.JOYSTICK_ERROR_ALLOWANCE) {
+		if(Math.abs(DSManager.getDriveYAxis()) <= Constants.JOYSTICK_ERROR_ALLOWANCE && DSManager.getCoDriveRawButtonOne() == false && OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)<5 && Math.abs(Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE) {
 			
 			OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_TELE);
 			
