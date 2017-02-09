97b24476df8e8d7e000e2677da82baec4b25e192
Tiburon <tiburonbenavides@gmail.com>
2017-02-08 18:27:17 -0600

    Working on distinguishing between Auton and Teleop

--- Subsystems/src/org/usfirst/frc/team930/robot/Drive.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Drive.java
@@ -3,27 +3,30 @@ package org.usfirst.frc.team930.robot;
 public class Drive implements Runnable {
 
 	public void run(){
-		
-	//	OutputManager.setDrivetrainPercentVbusMode();
-		
-		// Adjusting joystick sensitivity
-		double xValue = Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY);
-		double yValue = Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY);
-				
-		// Deadband
-		if (Math.abs(xValue) < Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(yValue) < Constants.JOYSTICK_ERROR_ALLOWANCE) {
-			xValue = Constants.X_DEFAULT_VALUE;
-			yValue = Constants.Y_DEFAULT_VALUE;
+		if(OutputManager.isRobotTeleop()){
+		//	OutputManager.setDrivetrainPercentVbusMode();
+			
+			// Adjusting joystick sensitivity
+			double xValue = Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY);
+			double yValue = Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY);
+					
+			// Deadband
+			if (Math.abs(xValue) < Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(yValue) < Constants.JOYSTICK_ERROR_ALLOWANCE) {
+				xValue = Constants.X_DEFAULT_VALUE;
+				yValue = Constants.Y_DEFAULT_VALUE;
+			}
+			else{
+				OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_DRIVE);
+			}
+					
+			// Setting talons
+			OutputManager.setSpeedL(yValue + xValue);
+			OutputManager.setSpeedR(yValue - xValue);
 		}
-		else{
-			OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_DRIVE);
+		else if (OutputManager.isRobotAuton()){
+			
 		}
-				
-		// Setting talons
-		OutputManager.setSpeedL(yValue + xValue);
-		OutputManager.setSpeedR(yValue - xValue);
-		
-		
+			
 		}
 	public static void init(){
 		OutputManager.setDrivetrainPercentVbusMode();		
