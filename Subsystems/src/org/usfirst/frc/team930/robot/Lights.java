96cfbdf2866fa1fd09e1bb832a490befa022171d
Tiburon <tiburonbenavides@gmail.com>
2017-02-08 19:20:38 -0600

    Added logic to Devin's illogical Lights code

--- Subsystems/src/org/usfirst/frc/team930/robot/Lights.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Lights.java
@@ -4,18 +4,22 @@ public class Lights implements Runnable {
 
 	public void run() {
 		//Makes a button to make the fun mode work in lights
-		if(DSManager.getCoDriveRawButtonFive()){
+		if (OutputManager.isRobotTeleop()){
+			if(DSManager.getCoDriveRawButtonFive()){
+				
+				OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_FUUN);
+				
+			}
 			
-			OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_FUUN);
+			//Sets the teleop lights when no other lights are running
+			if(Math.abs(DSManager.getDriveYAxis()) <= Constants.JOYSTICK_ERROR_ALLOWANCE && DSManager.getCoDriveRawButtonOne() == false && OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)<5 && Math.abs(Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE) {
+				
+				OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_TELE);
+				
+			}
 			
 		}
 		
-		//Sets the teleop lights when no other lights are running
-		if(Math.abs(DSManager.getDriveYAxis()) <= Constants.JOYSTICK_ERROR_ALLOWANCE && DSManager.getCoDriveRawButtonOne() == false && OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)<5 && Math.abs(Math.pow(DSManager.getDriveXAxis(), Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE && Math.abs(Math.pow(DSManager.getDriveYAxis() * -1.0, Constants.JOYSTICK_NONLINEARITY)) > Constants.JOYSTICK_ERROR_ALLOWANCE) {
-			
-			OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_TELE);
-			
-		}
 	}
 
 }
