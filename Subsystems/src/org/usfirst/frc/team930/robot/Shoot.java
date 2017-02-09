aa433507a19224a0be9c10783169f23039c2c611
Tiburon <tiburonbenavides@gmail.com>
2017-02-08 18:50:25 -0600

    Put paul's shooter code into architecture and eliminated consts

--- Subsystems/src/org/usfirst/frc/team930/robot/Shoot.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Shoot.java
@@ -7,66 +7,35 @@ public class Shoot implements Runnable {
 	double targetX;
 	double targetY;
 	double speed = 0.50;
-	double rpmActual = 0;
-	//double fGain = 0.0;
-	double kF = 0.0;
-	
-	boolean Apressed = false; 
-	boolean Bpressed = false;
-	boolean Ypressed = false;
-	
-	boolean onBool = false;
 	
 	public void run(){
-		/*
-		if (DSManager.getDriveRawButtonFour() && (!Ypressed)) { // Y
-			speed += 0.025;                      
-			Ypressed = true;
-		} else if((!DSManager.getDriveRawButtonFour()) && Ypressed) {
-			Ypressed = false;
-		}
-
-		if(DSManager.getDriveRawButtonOne() && (!Apressed)) { // A
-		    speed -= 0.025;  
-		    Apressed = true;
-		} else if((!DSManager.getDriveRawButtonOne()) && Apressed) {
-		    Apressed = false;
-		}
-
-		if(DSManager.getDriveRawButtonOne() && (!Bpressed)) { // B
-		    onBool = !onBool;
-		    Bpressed = true; 
-		} else if((!DSManager.getDriveRawButtonTwo()) && Bpressed) {
-			Bpressed = false;
-		}
-		*/
 		
-		if (DSManager.getDriveTriggers()){ // Replace with onBool for buttons
-			OutputManager.setShooterSpeedMode();
-			OutputManager.setShooterSpeed(speed*4500);
-		}
-		else{
-			OutputManager.setShooterDisabledMode();
-			OutputManager.setShooterSpeed(0.0);
-		}
+		if (OutputManager.isRobotTeleop()){
 		
-		//System.out.println("TS: " +  (speed * 4500) + "   S: " + (OutputManager.getTalonShooterMotor())); // ln
-		//System.out.println("     Volt: " + myTal.getOutputVoltage() + "    E: " + myTal.getClosedLoopError());
-		SmartDashboard.putNumber("Speed of Shooter", OutputManager.getTalonShooterMotor());
-		//if (onBool) {
-			//myTal.set((speed * 4550 * 4096.0 / 60.0 / 10.0));
+			if (DSManager.getDriveTriggers()){ // Replace with onBool for buttons
+				OutputManager.setShooterSpeedMode();
+				OutputManager.setShooterSpeed(speed * Constants.FULL_SHOOT_SPEED);
+			}
+			else{
+				OutputManager.setShooterDisabledMode();
+				OutputManager.setShooterSpeed(0.0);
+			}
 			
-		//}
-		
-		//System.out.println("Shoot " + Timer.getFPGATimestamp());
-		
-		//Setting Lights for Shooting
-		if(OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)>5){
-		
-			OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_SHOOT);
-		
-		}
+			SmartDashboard.putNumber("Speed of Shooter", OutputManager.getTalonShooterMotor());
 			
+			//Setting Lights for Shooting
+			if(OutputManager.getPDPChannelCurrent(Constants.PDP_CHANNEL7)>5){
+			
+				OutputManager.setLights(OutputManager.LightPatterns.LIGHTS_SHOOT);
+			
+			}
+			
+		}
+		
+		else if (OutputManager.isRobotAuton()){
+			
+		}
+		
 	}
 
 	
