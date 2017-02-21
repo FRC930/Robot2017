8d3abec0447cdf9475c6c0d1c871c2ea47e8b4c3
SamMurphy1 <samanthamurphy@wi.rr.com>
2017-02-20 21:00:57 -0600

    Auton

--- Subsystems/src/org/usfirst/frc/team930/robot/BackupAuton.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/BackupAuton.java
@@ -9,21 +9,31 @@ public class BackupAuton implements Runnable {
 	
 	@Override
 	public void run() {
-
-		time.start();
 		
-		OutputManager.setSpeedL(0.25);
-		OutputManager.setSpeedR(0.25);
+		if (OutputManager.isRobotAuton()) {
 		
-		if (time.get() > 3) {
-			OutputManager.setSpeedL(0);
-			OutputManager.setSpeedR(0);
-		}
+			if (time.get() > 3) {
 				
+				OutputManager.setSpeedL(0);
+				OutputManager.setSpeedR(0);
+			}
+			
+			else {
+				
+				OutputManager.setSpeedL(0.25);
+				OutputManager.setSpeedR(0.25);
+			}
+		
+		}
+		
 	}
 	
 	public static void init(){
-		time.reset();		
+		
+		time.reset();	
+		
+		time.start();
+	
 	}
 	
 
