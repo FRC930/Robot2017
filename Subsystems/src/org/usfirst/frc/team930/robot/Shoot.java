7490d2f8a62680f4414f882c168eca04bbc1a3a6
devin132 <milligandm@hotmail.com>
2017-02-28 19:01:26 -0600

    somone is touching my shooooootttter

--- Subsystems/src/org/usfirst/frc/team930/robot/Shoot.java
+++ Subsystems/src/org/usfirst/frc/team930/robot/Shoot.java
@@ -15,7 +15,7 @@ public class Shoot implements Runnable {
 			
 			if (DSManager.getCoDriveShootTrigger()){ // Replace with onBool for buttons
 				
-			/*	if(OutputManager.getFeedbackSpeed(OutputManager.Motors.SHOOTER) >= Constants.FULL_SHOOT_SPEED + Constants.BANG_BANG_VARIABLES){
+				if(OutputManager.getFeedbackSpeed(OutputManager.Motors.SHOOTER) >= Constants.FULL_SHOOT_SPEED + Constants.BANG_BANG_VARIABLES){
 					
 					OutputManager.setShooterSpeed(0.5);
 					
@@ -27,19 +27,17 @@ public class Shoot implements Runnable {
 					
 				}
 				
-				OutputManager.setShooterSpeed(shooterSpeed);
-				//OutputManager.setShooterSpeed(Constants.FULL_SHOOT_SPEED);
-<<<<<<< HEAD
-					
-=======
+				//OutputManager.setShooterSpeed(shooterSpeed);
+
+
 				
 			}
 			
-			else{
-				*/
+			/*else{
+				
 				OutputManager.setShooterSpeed(-0.8);	
 				
-			}
+			}*/
 			else {
 				//OutputManager.setShooterSpeedMode();
 				OutputManager.setShooterSpeed(0.0);
